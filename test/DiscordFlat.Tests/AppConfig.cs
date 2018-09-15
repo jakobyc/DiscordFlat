819fab881bda139f5db2e2dc18cd884fdffde012
Chad <chad3281@gmail.com>
2018-09-14 20:01:14 -0400

    Added test project

---
+++ test/DiscordFlat.Tests/AppConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordFlat.Tests
+{
+    public class AppConfig
+    {
+        public static IConfiguration Config { get; } = Init();
+
+        private static IConfiguration Init()
+        {
+            return new ConfigurationBuilder().AddJsonFile("appsettings.test.json")
+                                             .Build();
+        }
+    }
+}
---
+++ test/DiscordFlat.Tests/WebSocketTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace DiscordFlat.Tests
+{
+    [TestClass]
+    public class WebSocketTests
+    {
+        [TestMethod]
+        public void Connect()
+        {
+        }
+    }
+}
